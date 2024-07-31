e1f9517a6e0d82aa98ae91e73014f88615f46b5f
AbhikPatel <abhikpatel15@gmail.com>
2024-07-31 08:45:15 +0530

    feat(Update): Added Heading
    Adding location, name and mobile number

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,7 +1,8 @@
 const Header = () => {
   return (
-    <header className="h-100 d-flex align-items-center px-3">
-      <h2 className="m-0"> Ambica Acupressure Center</h2>
+    <header className="h-100 d-flex align-items-center px-3 flex-column">
+      <h2 className="m-1"> Ambica Acupressure Center</h2>
+      <h6 style={{fontSize: '0.8rem'}}> Valsad &#8226; Kishor Patel &#8226; 98257-96426 </h6>
     </header>
   )
 }
